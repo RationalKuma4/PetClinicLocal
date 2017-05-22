1db12de56e31215c78128bd5c8864ef237c75c74
Anselmo Alvarado <ans.alv.46@gmail.com>
2017-05-22 13:38:32 -0500

    Menu

--- PetClinicLocal/PetClinicLocal/Views/MasterMenuPage.xaml.cs
+++ PetClinicLocal/PetClinicLocal/Views/MasterMenuPage.xaml.cs
@@ -16,23 +16,17 @@ namespace PetClinicLocal.Views
             InitializeComponent();
 
             MenuList = new List<MasterPageItem>();
-
-            // Creating our pages for menu navigation
-            // Here you can define title for item, 
-            // icon on the left side, and page that you want to open after selection
+            
             var page1 = new MasterPageItem { Title = "Veterinarios", Icon = "surre.png", TargetType = typeof(MainCitaPage) };
             var page2 = new MasterPageItem { Title = "Mascotas", Icon = "paww.png", TargetType = typeof(TabbedMascotaPage) };
             var page3 = new MasterPageItem { Title = "Citas", Icon = "cale.png", TargetType = typeof(MainVeterinarioPage) };
-
-            // Adding menu items to menuList
+            
             MenuList.Add(page1);
             MenuList.Add(page2);
             MenuList.Add(page3);
-
-            // Setting our list to be ItemSource for ListView in MainPage.xaml
+            
             navigationDrawerList.ItemsSource = MenuList;
-
-            // Initial navigation, this can be used for our home page
+            
             Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(MainCitaPage)));
         }
 
